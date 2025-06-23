8f19e61e172b42fb3bc57ffe57a8f24f3cfc3ccb
Xardas1 <ciufel.maly@gmail.com>
2025-06-23 16:07:14 +0200

    olek

--- Soltys/frontend/src/components/FacebookPage.jsx
+++ Soltys/frontend/src/components/FacebookPage.jsx
@@ -11,7 +11,7 @@ const FacebookPage = () => {
         const password = e.target[1].value;
 
         try {
-            const response = await fetch("http://localhost:8000/login/", {
+            const response = await fetch("https://projektdawid-1.onrender.com/login/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
